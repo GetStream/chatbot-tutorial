6cc814aa4767341c8aa1a898f00fd8ae6973d814
Thierry Schellenbach <thierryschellenbach@gmail.com>
2020-01-11 12:01:03 -0700

    bit more cleanup

--- chat-agent-dashboard/src/App.js
+++ chat-agent-dashboard/src/App.js
@@ -1,57 +1,61 @@
-import React from 'react';
-import { Chat, Channel, ChannelHeader, Thread, Window } from 'stream-chat-react';
-import { MessageList,ChannelList, TypingIndicator, MessageInputFlat, MessageCommerce, MessageInput } from 'stream-chat-react';
-import { StreamChat, DevToken } from 'stream-chat';
+import React from "react";
+import {
+  Chat,
+  Channel,
+  ChannelHeader,
+  Thread,
+  Window
+} from "stream-chat-react";
+import {
+  MessageList,
+  ChannelList,
+  TypingIndicator,
+  MessageInputFlat,
+  MessageCommerce,
+  MessageInput
+} from "stream-chat-react";
+import { StreamChat, DevToken } from "stream-chat";
 
-import 'stream-chat-react/dist/css/index.css';
+import "stream-chat-react/dist/css/index.css";
 
 const chatClient = new StreamChat(process.env.REACT_APP_STREAM_API_KEY);
 
 chatClient.setUser(
   {
-       id: 'support-agent-123',
-       name: 'Jessica',
-       image: 'https://getstream.io/random_svg/?id=support-agent-123&name=Jessica'
+    id: "support-agent-123",
+    name: "Jessica",
+    image: "https://getstream.io/random_svg/?id=support-agent-123&name=Jessica"
   },
-  chatClient.devToken('support-agent-123'),
+  chatClient.devToken("support-agent-123")
 );
 
-const filters = { type: 'commerce' };
+const filters = { type: "commerce" };
 const sort = { last_message_at: -1 };
-const channels = chatClient.queryChannels(filters, sort, {watch: true, presence: true});
+const channels = chatClient.queryChannels(filters, sort, {
+  watch: true,
+  presence: true
+});
 
+function App() {
+  return (
+    <div>
+      <Chat client={chatClient} theme={"commerce light"}>
+        <ChannelList filters={filters} sort={sort} />
+        <Channel>
+          <Window>
+            <ChannelHeader />
 
+            <MessageList
+              TypingIndicator={TypingIndicator}
+              Message={MessageCommerce}
+            />
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-
-  render() {
-    return (
-      <div>
-        <Chat client={chatClient} theme={'commerce light'}>
-          <ChannelList
-            filters={filters}
-            sort={sort}
-          />
-          <Channel>
-            <Window>
-              <ChannelHeader />
-
-                <MessageList
-                  TypingIndicator={TypingIndicator}
-                  Message={MessageCommerce}
-                 />
-
-              <MessageInput Input={MessageInputFlat} />
-            </Window>
-          </Channel>
-        </Chat>
-      </div>
-    );
-  }
+            <MessageInput Input={MessageInputFlat} />
+          </Window>
+        </Channel>
+      </Chat>
+    </div>
+  );
 }
 
 export default App;
